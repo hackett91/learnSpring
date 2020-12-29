7cd44ec08f5b5892e9065ab1ba6d5c7b9aaf3c6a
hackett91 <C16723031@mydit.ie>
2020-12-29 23:06:09 +0000

    learning internationlized messages

--- src/main/java/com/capcon/rest/webservices/orderingservice/Controller/HelloWorldController.java
+++ src/main/java/com/capcon/rest/webservices/orderingservice/Controller/HelloWorldController.java
@@ -1,7 +1,13 @@
 package com.capcon.rest.webservices.orderingservice.Controller;
 
 import com.capcon.rest.webservices.orderingservice.Model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller - tell spring it will be handling requests
 @RestController
@@ -9,8 +15,8 @@ public class HelloWorldController {
     // GET
     // URI - /hello-world
     // Method - "Hello World"
-
-
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -25,5 +31,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("hello world, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
--- src/main/java/com/capcon/rest/webservices/orderingservice/OrderingServiceApplication.java
+++ src/main/java/com/capcon/rest/webservices/orderingservice/OrderingServiceApplication.java
@@ -2,6 +2,10 @@ package com.capcon.rest.webservices.orderingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import java.util.Locale;
 
 @SpringBootApplication
 public class OrderingServiceApplication {
@@ -10,4 +14,10 @@ public class OrderingServiceApplication {
 		SpringApplication.run(OrderingServiceApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
