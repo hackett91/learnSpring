b3fd6336a8af87d38241eca8ff63cf712c8a693f
hackett91 <C16723031@mydit.ie>
2020-12-29 19:05:12 +0000

    validation and hateoas

--- src/main/java/com/capcon/rest/webservices/orderingservice/Exception/CustomisedResponseEntityExceptionHandler.java
+++ src/main/java/com/capcon/rest/webservices/orderingservice/Exception/CustomisedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.capcon.rest.webservices.orderingservice.Exception;
 
 import com.capcon.rest.webservices.orderingservice.Model.User.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +30,10 @@ public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExce
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed",ex.getBindingResult().toString());
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);    }
 }
